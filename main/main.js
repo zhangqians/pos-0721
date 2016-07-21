ba10f4da019109502150c7b411317e74027ce85d
Peng.Li <peng.li@rea-group.com>
2016-07-22 00:34:55 +0800

    all tests passed

--- main/main.js
+++ main/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-//
-// countBarcodes
-// buildCartItems
-// buildPromotedItems
-// calculateTotalPrices
-// buildReceipt
-// printReceiptString
-
 function formatTags(tags) {
   return tags.map((tag) => {
     if (tag.includes('-')) {
@@ -23,4 +15,118 @@ function formatTags(tags) {
       }
     }
   })
+}
+
+function _getExistentElementByBarcode(array, barcode) {
+  return array.find((element) => element.barcode === barcode);
+}
+
+function countBarcodes(formattedTags) {
+  return formattedTags.reduce((result, formattedTag) => {
+    let found = _getExistentElementByBarcode(result, formattedTag.barcode);
+    if (found) {
+      found.count += formattedTag.count;
+    } else {
+      result.push(formattedTag);
+    }
+    return result;
+  }, []);
+}
+
+function buildCartItems(countedBarcodes, allItems) {
+  return countedBarcodes.map(({barcode, count}) => {
+    let item = _getExistentElementByBarcode(allItems, barcode);
+    return {
+      barcode,
+      name: item.name,
+      unit: item.unit,
+      price: item.price,
+      count
+    }
+  })
+}
+
+function _fixFloat(number) {
+  return parseFloat(number.toFixed(2))
+}
+
+function buildPromotedItems(cartItems, promotions) {
+  let promotion = promotions.find((promotion)=> promotion.type === '单品批发价出售');
+  return cartItems.map((cartItem)=> {
+    let hasPromoted = promotion.barcodes.includes(cartItem.barcode) && cartItem.count > 10;
+    let totalPrice = cartItem.price * cartItem.count;
+    let saved = hasPromoted ? totalPrice * 0.05 : 0;
+    let payPrice = totalPrice - saved;
+    return Object.assign({}, cartItem, {
+      payPrice, saved: _fixFloat(saved)
+    })
+  })
+}
+
+function calculateTotalPrices(promotedItems) {
+  return promotedItems.reduce((result, {payPrice, saved}) => {
+    result.totalPayPrice += payPrice;
+    result.totalSaved += saved;
+    return result;
+  }, {totalPayPrice: 0, totalSaved: 0})
+}
+
+function buildReceipt(promotedItems, totalPrices) {
+  let savedItems = promotedItems.filter((promotedItem) => promotedItem.saved > 0)
+    .map(({name, count, unit}) => {
+      return {name, count, unit}
+    });
+  return {
+    promotedItems: promotedItems.map(({name, unit, price, count, payPrice, saved}) => {
+      return {name, unit, price, count, payPrice, saved}
+    }),
+    savedItems,
+    totalPayPrice: totalPrices.totalPayPrice,
+    totalSaved: totalPrices.totalSaved
+  }
+
+}
+
+function buildReceiptString(receipt) {
+  let lines = ['***<没钱赚商店>购物清单***'];
+  receipt.promotedItems.forEach(({name, count, unit, price, payPrice, saved})=> {
+    let line = `名称：${name}，数量：${count}${unit}，单价：${price.toFixed(2)}(元)，小计：${payPrice.toFixed(2)}(元)`;
+    if (saved > 0) {
+      line += `，优惠：${saved.toFixed(2)}(元)`
+    }
+    lines.push(line)
+  });
+
+
+  let hasSaved = receipt.savedItems.length > 0;
+  if (hasSaved) {
+    lines.push('----------------------');
+    lines.push('批发价出售商品：');
+    receipt.savedItems.forEach(({name, count, unit}) => {
+      lines.push(`名称：${name}，数量：${count}${unit}`);
+    });
+  }
+
+  lines.push('----------------------');
+  lines.push(`总计：${receipt.totalPayPrice.toFixed(2)}(元)`);
+  if (hasSaved) {
+    lines.push(`节省：${receipt.totalSaved}(元)`);
+  }
+  lines.push('**********************');
+
+  let receiptString = lines.join('\n');
+  console.log(receiptString);
+}
+
+function printReceipt(tags) {
+  let formattedTags = formatTags(tags);
+  let countedBarcodes = countBarcodes(formattedTags);
+  let allItems = loadAllItems();
+  let cartItems = buildCartItems(countedBarcodes, allItems);
+  let promotions = loadPromotions();
+  let promotedItems = buildPromotedItems(cartItems, promotions);
+  let totalPrices = calculateTotalPrices(promotedItems);
+  let receipt = buildReceipt(promotedItems, totalPrices);
+  let receiptString = buildReceiptString(receipt);
+  console.log(receiptString);
 }
--- test/main-test.js
+++ test/main-test.js
@@ -3,29 +3,142 @@
 describe('pos', () => {
 
   it('should format the tags', ()=> {
-    let tags = [
-      'ITEM0001',
-      'ITEM0002-3'
-    ];
+    let tags = ['ITEM0001', 'ITEM0002-3'];
     let formattedTags = formatTags(tags);
     let expected = [
-      {
-        barcode: 'ITEM0001',
-        count: 1
-      },
-      {
-        barcode: 'ITEM0002',
-        count: 3
-      }
+      {barcode: 'ITEM0001', count: 1},
+      {barcode: 'ITEM0002', count: 3}
     ];
     expect(formattedTags).toEqual(expected);
   });
 
-  it('should print text for items has promoted', () => {
-
-    const tags = [
-      // 根据输出自行推断出合格的输入
+  it('countBarcodes', ()=> {
+    let formattedTags = [
+      {barcode: 'ITEM0001', count: 1},
+      {barcode: 'ITEM0002', count: 3},
+      {barcode: 'ITEM0001', count: 1}
     ];
+    let countedBarcodes = countBarcodes(formattedTags);
+    let expected = [
+      {barcode: 'ITEM0001', count: 2},
+      {barcode: 'ITEM0002', count: 3}
+    ];
+    expect(countedBarcodes).toEqual(expected);
+  });
+
+  it('buildCartItems', ()=> {
+    let allItems = loadAllItems();
+    let countedBarcodes = [
+      {barcode: 'ITEM000000', count: 2},
+      {barcode: 'ITEM000001', count: 3}
+    ];
+    let cartItems = buildCartItems(countedBarcodes, allItems);
+    let expected = [
+      {barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 2},
+      {barcode: 'ITEM000001', name: '羽毛球', unit: '个', price: 1.00, count: 3}
+    ];
+    expect(cartItems).toEqual(expected);
+  });
+
+  it('buildPromotedItems', ()=> {
+    let cartItems = [
+      {barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 11},
+      {barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 10}
+    ];
+    let promotions = loadPromotions();
+    let promotedItems = buildPromotedItems(cartItems, promotions);
+    let expected = [
+      {
+        barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 11,
+        payPrice: 31.35, saved: 1.65
+      },
+      {
+        barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 10,
+        payPrice: 30, saved: 0
+      }
+    ];
+    expect(promotedItems).toEqual(expected);
+  });
+
+  it('calculateTotalPrices', () => {
+    let promotedItems = [
+      {
+        barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 11,
+        payPrice: 31.35, saved: 1.65
+      },
+      {
+        barcode: 'ITEM000001', name: '羽毛球', unit: '个', price: 1.00, count: 3,
+        payPrice: 3, saved: 0
+      }
+    ];
+    let totalPrices = calculateTotalPrices(promotedItems);
+    let expected = {
+      totalPayPrice: 34.35,
+      totalSaved: 1.65
+    };
+    expect(totalPrices).toEqual(expected);
+  });
+
+  it('buildReceipt without saved', () => {
+    let promotedItems = [
+      {
+        barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 10,
+        payPrice: 30, saved: 0
+      }
+    ];
+    let totalPrices = {
+      totalPayPrice: 30,
+      totalSaved: 0
+    };
+    let receipt = buildReceipt(promotedItems, totalPrices);
+    let expected = {
+      promotedItems: [
+        {
+          name: '可口可乐', unit: '瓶', price: 3.00, count: 10, payPrice: 30, saved: 0
+        }
+      ],
+      savedItems: [],
+      totalPayPrice: 30,
+      totalSaved: 0
+    };
+    expect(receipt).toEqual(expected);
+  });
+
+  it('buildReceipt with saved items', () => {
+    let promotedItems = [
+      {
+        barcode: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, count: 11,
+        payPrice: 31.35, saved: 1.65
+      }
+    ];
+    let totalPrices = {
+      totalPayPrice: 31.35,
+      totalSaved: 1.65
+    };
+    let receipt = buildReceipt(promotedItems, totalPrices);
+    let expected = {
+      promotedItems: [
+        {
+          name: '可口可乐', unit: '瓶', price: 3.00, count: 11,
+          payPrice: 31.35, saved: 1.65
+        }
+      ],
+      savedItems: [{
+        name: '可口可乐',
+        count: 11,
+        unit: '瓶'
+      }],
+      totalPayPrice: 31.35,
+      totalSaved: 1.65
+    };
+    expect(receipt).toEqual(expected);
+  });
+
+  it('should print text for items has promoted', () => {
+    let tags = [
+      'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000', 'ITEM000000',
+      'ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000001',
+      'ITEM000002-2'];
 
     spyOn(console, 'log');
 
@@ -48,9 +161,10 @@ describe('pos', () => {
 
   it('should print text for items with no one has promoted', () => {
 
-    const tags = [
-      // 根据输出自行推断出合格的输入
-    ];
+    let tags = [
+      'ITEM000000', 'ITEM000000', 'ITEM000000',
+      'ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000001',
+      'ITEM000002-2'];
 
     spyOn(console, 'log');
 
